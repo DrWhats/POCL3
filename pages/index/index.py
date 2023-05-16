3d17e639121cbe926434b3203935da4d3b7ca58e
DrWhats <hudorojckov.leva@gmail.com>
2023-05-16 20:05:32 +0500

    Изменена главная страница

--- pages/index/index.py
+++ pages/index/index.py
@@ -8,19 +8,4 @@ blueprint = Blueprint('index', __name__, template_folder='templates')
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    form = QuestionsForm()
-    if form.validate_on_submit():
-        fio = form.fio.data
-        email = form.email.data
-        phonenumber = form.phonenumber.data
-        shortdescribe = form.shortdescribe.data
-        question = form.question.data
-        print(fio)
-        print(email)
-        print(phonenumber)
-        print(shortdescribe)
-        print(question)
-        return redirect(url_for('index.index'))
-    else:
-        flash("Invalid username or password.", 'error')
-    return render_template('question.html', form = form)
+    return render_template('index.html')
