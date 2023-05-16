fe6ec8e287e3e9595d2b2850d7848cdf5e805f05
DrWhats <hudorojckov.leva@gmail.com>
2023-05-16 20:09:04 +0500

    index fix

--- pages/index/index.py
+++ pages/index/index.py
@@ -6,6 +6,7 @@ from pages.forms import QuestionsForm
 blueprint = Blueprint('index', __name__, template_folder='templates')
 
 
+@blueprint.route('/index', methods=['GET', 'POST'])
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
