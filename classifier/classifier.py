59873b9c1be2c75e579684d293767d7038a1c39c
Dr_Wat <50990387+DrWhats@users.noreply.github.com>
2023-05-21 20:24:01 +0500

    Update classifier.py

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,13 +1,25 @@
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# import pytest
+model_checkpoint = 'cointegrated/rubert-base-cased-nli-threeway'
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
+if torch.cuda.is_available():
+    model.cuda()
 
-def classify(text=None):
 
-    classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli") # Изменить модель
+def predict_zero_shot(text, label_texts, model, tokenizer, label='entailment', normalize=True):
+    label_texts
+    tokens = tokenizer([text] * len(label_texts), label_texts, truncation=True, return_tensors='pt', padding=True)
+    with torch.inference_mode():
+        result = torch.softmax(model(**tokens.to(model.device)).logits, -1)
+    proba = result[:, model.config.label2id[label]].cpu().numpy()
+    if normalize:
+        proba /= sum(proba)
+    return proba
 
-    candidate_labels = ["Учетная запись", "РПД",
-                        "Учебные планы", "Личный кабинет"]
 
-    output = classifier(text, candidate_labels, multi_label=False, use_fast=False)
-    return output["labels"][0], output["scores"][0]
+if __name__ == "__main__":
+    classes = ['Я доволен', 'Я недоволен']
+    print(predict_zero_shot('Какая гадость эта ваша заливная рыба!', classes, model, tokenizer))
+    print(predict_zero_shot('Какая вкусная эта ваша заливная рыба!', classes, model, tokenizer))
