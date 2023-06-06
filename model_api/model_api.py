e55fe8ee1ae071e1d7e46ee1d9fb72e5a9bbb52b
GolikovAndrey <and.golikoff@yandex.ru>
2023-06-06 20:34:19 +0500

    fix

--- model_api/model_api.py
+++ model_api/model_api.py
@@ -4,6 +4,6 @@ from .classifier import predict
 app = FastAPI()
 
 @app.post("/model_request")
-async def model_request(text: str):
-    prediction = predict(text)
+async def model_request(text: dict):
+    prediction = predict(text['question'])
     return {"prediction": prediction}
