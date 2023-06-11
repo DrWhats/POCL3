d656269ae85f3fdeba585a33cadaf53457b50351
Margarita Onuchina <116965536+Margarita-Onuchina@users.noreply.github.com>
2023-06-11 16:02:16 +0500

    заполнение базы данных

--- model_api/model_api.py
+++ model_api/model_api.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from classifier import predict
+import database
 
 app = FastAPI()
 
 @app.post("/model_request")
 async def model_request(text: dict):
     prediction = predict(text['question'])
+    database.update_request_label(prediction, text['question'])
     return {"prediction": prediction}
