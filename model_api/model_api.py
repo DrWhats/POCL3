88ca82ceddb18c3073c82f02d0bfbdaf9fb7e31c
DrWhats <hudorojckov.leva@gmail.com>
2023-06-18 18:34:52 +0500

    temp

---
+++ model_api/model_api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import bot
+from classifier import predict
+import database
+
+app = FastAPI()
+
+
+@app.post("/model_request")
+async def model_request(text: dict):
+    prediction = predict(text['question'])
+    database.update_request_label(prediction, text['question'])
+    label_id = database.get_type_by_label(prediction)
+    moders = database.get_type_moders(label_id)
+    bot.send_notification_to_users(moders, "Aloha!")
+
