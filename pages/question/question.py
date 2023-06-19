1f7bf36e4447281fce0fa3fd71e27efb6c4ae3d2
Dr_Wat <hudorojckov.leva@gmail.com>
2023-06-19 10:16:47 +0500

    Revert "temp"
    This reverts commit 88ca82ceddb18c3073c82f02d0bfbdaf9fb7e31c.

--- pages/question/question.py
+++ pages/question/question.py
@@ -34,18 +34,11 @@ def question():
 
         db.session.add(new_request)
         db.session.commit()
-<<<<<<< Updated upstream:pages/question/question.py
         question_type = predict(question)
         type_id = db.session.query(Types.id).filter(Types.type == question_type)
         current_requset = db.session.execute(db.session.query(Request).filter(Request.question == question))
         current_requset.typeId = type_id
         db.session.commit()
-=======
-        luboy_slovarik = {"question": question}
-        response = requests.post(url="http://127.0.0.1:8000/model_request",
-                                 json=luboy_slovarik,
-                                 headers={"Content-Type": "application/json"})
->>>>>>> Stashed changes:flask_app/pages/question/question.py
         return render_template('question_success.html')
     else:
         flash("Invalid username or password.", 'error')
